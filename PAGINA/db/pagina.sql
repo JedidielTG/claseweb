154f71c9cfc78d0945a627b1b18162451a1f4c11
JedidielTG <jedimf5@gmail.com>
2023-08-19 21:58:41 -0600

    commit inicial

--- PAGINA/db/pagina.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE pagina( id_usuario INT AUTO_INCREMENT PRIMARY KEY ,
-            Procesador VARCHAR(150),
-            caracteristicas VARCHAR(50),
-            marca VARCHAR (50),
-            precio VARCHAR (50) );
